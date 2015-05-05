1403f383f1c4d90ecf0fa51c0d232b4aabbcee2f
Aleksuei Riabtsev <aleksuei.riabtsev@ec.gc.ca>
2015-05-05 09:21:20 -0400

    chore(grunt): first commit

---
+++ build.sh
@@ -0,0 +1,26 @@
+﻿#!/bin/bash
+
+wetCore="https://github.com/wet-boew/wet-boew.git#4.0.5"
+
+# enable error reporting to the console, just in case
+set -e
+
+# only proceed script when started not by pull request (PR)
+if [ $TRAVIS_PULL_REQUEST == "true" ]; then
+    echo "this is PR, exiting"
+    exit 0
+fi
+
+# ignore tags
+if [ ! -z $TRAVIS_TAG ]; then
+    echo "this is a tag, exiting"
+    exit 0
+fi
+
+git clone -b $wetCore
+
+cd web-boew
+
+npm install
+
+grunt
